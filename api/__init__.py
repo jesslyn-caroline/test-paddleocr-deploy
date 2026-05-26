ea31eec09ec09859174cd163d3899ee32e0dfcfa
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2026-05-27 00:53:16 +0700

    Enable index

--- api/__init__.py
+++ api/__init__.py
@@ -5,14 +5,14 @@ def create_app():
     app = Flask(__name__)
     CORS(app)
 
-    # from api.routes.index import init_index_route
+    from api.routes.index import init_index_route
     # from api.routes.revenue import init_revenue_route
     # from api.routes.demand import init_demand_route
     # from api.routes.bundling import init_bundling_route
     # from api.routes.blur import init_blur_route
     from api.routes.text_extraction import init_text_extraction_route
 
-    # init_index_route(app)
+    init_index_route(app)
     # init_revenue_route(app)
     # init_demand_route(app)
     # init_bundling_route(app)
